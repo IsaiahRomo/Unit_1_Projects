9751229b05cda0e99d3344e7bc9814359ed1ec86
Isaiah Romo <iromo237@west-mec.org>
2020-01-16 16:55:28 -0700

    Finished case 1

---
+++ case1/sd_mapper.js
@@ -0,0 +1,20 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: Isaiah Romo
+   Date:   1/16/20
+
+*/
+var thisTime = new Date();
+var timeStr = thisTime.toLocaleString();
+document.getElementById("timeStamp").innerHTML = timeStr;
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+var mapNum = (2 * thisMonth + thisHour)% 24;
+// insert the mapNum value to the img so that it select a certain img at that time
+var imgStr = "<img src='sd_sky" + mapNum +  ".png' />";
+// insert the value imgStr directly after the element's opening tag
+document.getElementById("planisphere").insertAdjacentHTML("afterbegin", imgStr);
